11e594f85482c2c424f7d7f1036ce192c4950acb
Matt Olson <targz3@yahoo.com>
2011-09-21 17:23:10 -0700

    Added a User factory and tests for the show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   
-  def new
-    @title = "Sign up"
+  def show
+   @user = User.find(params[:id])
   end
   
-  def show
-    @user = User.find(params[:id])
+  def new
+    @title = "Sign up"
   end
 end
